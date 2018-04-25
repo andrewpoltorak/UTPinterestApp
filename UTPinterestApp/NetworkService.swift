d9b2c6a9a46eb83ac6dc72ed7e6d3551ff8ac78f
Andrei Poltorak <andipoltorak13@gmail.com>
2018-04-25 19:40:23 +0300

    Add change to NetworkService

--- UTPinterestApp/NetworkService.swift
+++ UTPinterestApp/NetworkService.swift
@@ -9,39 +9,42 @@
 import Foundation
 import PinterestSDK
 
-//MARK: Properties
+class NetworkService {
 
-var viewControllerAuthorization = ViewControllerAuthorization()
-
-//MARK: Metods
-
-func authenticateUser() {
-    PDKClient.sharedInstance().authenticate(withPermissions: [PDKClientReadPublicPermissions,PDKClientWritePublicPermissions,PDKClientReadRelationshipsPermissions,PDKClientWriteRelationshipsPermissions], from: viewControllerAuthorization, withSuccess: { (PDKResponseObject) in
-        viewControllerAuthorization.activityIndicator.stopAnimating()
-        viewControllerAuthorization.accessToken = PDKClient.sharedInstance().oauthToken
-        userData()
-    }, andFailure: {
-        (Error) in
-        viewControllerAuthorization.view.isUserInteractionEnabled = true
-        viewControllerAuthorization.startLabel.text = "Please try again..."
-        viewControllerAuthorization.activityIndicator.stopAnimating()
-    })
-}
-
-func userData() {
-    let parameters: [String : String] = ["fields": "first_name,id,last_name,url,image,username,bio,counts,created_at,account_type"]
-    PDKClient.sharedInstance().getPath("/v1/me/", parameters: parameters, withSuccess: {
-        (PDKResponseObject) in
-        var viewControllerPersonalData = ViewControllerPersonalData()
-        viewControllerPersonalData = viewControllerAuthorization.story.instantiateViewController(withIdentifier: "ViewControllerPersonalData") as! ViewControllerPersonalData
-        viewControllerAuthorization.navigationController!.pushViewController(viewControllerPersonalData, animated: true)
-        viewControllerAuthorization.user = (PDKResponseObject?.user())!
-        _ = viewControllerPersonalData.view
-        viewControllerPersonalData.greetingLabel.text = "Welcome " + (PDKResponseObject?.user().firstName)! + " " + (PDKResponseObject?.user().lastName != nil ? (PDKResponseObject?.user().lastName)! : "")
-    }) {
-        (Error) in
-        viewControllerAuthorization.view.isUserInteractionEnabled = true
-        viewControllerAuthorization.startLabel.text = "Try again..."
-        viewControllerAuthorization.activityIndicator.stopAnimating()
+    //MARK: Properties
+    
+    var viewControllerAuthorization = ViewControllerAuthorization()
+    
+    //MARK: Metods
+    
+    func authenticateUser() {
+        PDKClient.sharedInstance().authenticate(withPermissions: [PDKClientReadPublicPermissions,PDKClientWritePublicPermissions,PDKClientReadRelationshipsPermissions,PDKClientWriteRelationshipsPermissions], from: self.viewControllerAuthorization, withSuccess: { (PDKResponseObject) in
+            self.viewControllerAuthorization.activityIndicator.stopAnimating()
+            self.viewControllerAuthorization.accessToken = PDKClient.sharedInstance().oauthToken
+            self.userData()
+        }, andFailure: {
+            (Error) in
+            self.viewControllerAuthorization.view.isUserInteractionEnabled = true
+            self.viewControllerAuthorization.startLabel.text = "Please try again..."
+            self.viewControllerAuthorization.activityIndicator.stopAnimating()
+        })
+    }
+    
+    func userData() {
+        let parameters: [String : String] = ["fields": "first_name,id,last_name,url,image,username,bio,counts,created_at,account_type"]
+        PDKClient.sharedInstance().getPath("/v1/me/", parameters: parameters, withSuccess: {
+            (PDKResponseObject) in
+            var viewControllerPersonalData = ViewControllerPersonalData()
+            viewControllerPersonalData = self.viewControllerAuthorization.story.instantiateViewController(withIdentifier: "ViewControllerPersonalData") as! ViewControllerPersonalData
+            self.viewControllerAuthorization.navigationController!.pushViewController(viewControllerPersonalData, animated: true)
+            self.viewControllerAuthorization.user = (PDKResponseObject?.user())!
+            _ = viewControllerPersonalData.view
+            viewControllerPersonalData.greetingLabel.text = "Welcome " + (PDKResponseObject?.user().firstName)! + " " + (PDKResponseObject?.user().lastName != nil ? (PDKResponseObject?.user().lastName)! : "")
+        }) {
+            (Error) in
+            self.viewControllerAuthorization.view.isUserInteractionEnabled = true
+            self.viewControllerAuthorization.startLabel.text = "Try again..."
+            self.viewControllerAuthorization.activityIndicator.stopAnimating()
+        }
     }
 }
